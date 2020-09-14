b186d45ed2f532163b542d7570dd11f86ec78ca9
Immodal <kychin.n@gmail.com>
2020-09-14 16:41:00 +1000

    Fixed bugs and implemented tests for neural network

--- src/neural-network/neural-network.js
+++ src/neural-network/neural-network.js
@@ -22,8 +22,8 @@ NeuralNetwork = (nInputs, nHidden, nOutputs, learningRate=0.1) => {
   nn.hoWeights = math.random([nOutputs, nHidden], -1, 1)
 
   // Biases for each layer
-  nn.hBias = math.ones([nHidden,1])
-  nn.oBias = math.ones([nOutputs,1])
+  nn.ihBias = math.ones([nHidden,1])
+  nn.hoBias = math.ones([nOutputs,1])
 
   /**
    * 
@@ -31,9 +31,9 @@ NeuralNetwork = (nInputs, nHidden, nOutputs, learningRate=0.1) => {
    */
   nn.predict = input => {
     // Output of input through hidden layer
-    const ihOut = nn.feedForward(input, nn.ihWeights, nn.hBias)
+    const ihOut = nn.feedForward(input, nn.ihWeights, nn.ihBias)
     // Output of hidden layer through output layer
-    return nn.feedForward(ihOut, nn.hoWeights, nn.oBias)
+    return nn.feedForward(ihOut, nn.hoWeights, nn.hoBias)
   }
 
   /**
@@ -60,7 +60,7 @@ NeuralNetwork = (nInputs, nHidden, nOutputs, learningRate=0.1) => {
     // Add bias to weighted values
     const biased = math.add(weighted, bias)
     // Run each node output through activation function
-    return biased.map(row => row.map(a.activation))
+    return biased.map(row => row.map(nn.activation))
   }
 
   /**
@@ -70,29 +70,29 @@ NeuralNetwork = (nInputs, nHidden, nOutputs, learningRate=0.1) => {
    */
   nn.train = (input, target) => {
     // Output of input through hidden layer
-    const ihOut = nn.feedForward(input, nn.ihWeights, nn.hBias)
+    const ihOut = nn.feedForward(input, nn.ihWeights, nn.ihBias)
     // Output of hidden layer through output layer
-    const hoOut =  nn.feedForward(ihOut, nn.hoWeights, nn.oBias)
+    const hoOut =  nn.feedForward(ihOut, nn.hoWeights, nn.hoBias)
 
     // Difference between prediction and target
     const hoOutError = math.subtract(hoOut, target)
     // gradient = (lr * derivative of feedforward output of current layer * error)
     const hoGradient = math.multiply(hoOutError.map(row => row.map(Activations.dSigmoid)), nn.lr)
     // delta weights = gradient * transpose(feedforward output of previous layer)
-    const hoWeightsDelta = math.multiply(hoGradient, matrix.transpose(ihOut))
+    const hoWeightsDelta = math.multiply(hoGradient, math.transpose(ihOut))
 
     // Calculate the contribution of each node toward the error of this layer via their weights
-    const ihOutError = math.multiply(matrix.transpose(nn.hoWeights), hoOutError)
+    const ihOutError = math.multiply(math.transpose(nn.hoWeights), hoOutError)
     const ihGradient = math.multiply(ihOutError.map(row => row.map(Activations.dSigmoid)), nn.lr)
-    const ihWeightsDelta = math.multiply(ihGradient, matrix.transpose(input))
+    const ihWeightsDelta = math.multiply(ihGradient, math.transpose(input))
 
     // Update weights at the very end so error contribution calculations are accurate
     nn.hoWeights = math.add(nn.hoWeights, hoWeightsDelta)
     // Update bias, the weight of bias is always 1, so the change is only the gradient
-    nn.oBias = math.add(nn.oBias, hoGradient)
+    nn.hoBias = math.add(nn.hoBias, hoGradient)
 
     nn.ihWeights = math.add(nn.ihWeights, ihWeightsDelta)
-    nn.hBias = math.add(nn.hBias, ihGradient)
+    nn.ihBias = math.add(nn.ihBias, ihGradient)
   }
 
   return nn
--- test/neural-network/neural-network-tests.js
+++ test/neural-network/neural-network-tests.js
@@ -1,3 +1,76 @@
 NeuralNetworkTests = {
-  
+  "toInput": () => {
+    const nn = NeuralNetwork(3, 2, 1)
+    const arr = [1,2,3]
+    const exp = [[1],[2],[3]]
+    nn.toInput(arr).forEach((row, i) => {
+      eq(exp[i].length, row.length)
+      row.forEach((x, j) => eq(exp[i][j], x))
+    })
+  },
+
+  "feedForward": () => {
+    const nn = NeuralNetwork(3, 2, 2)
+
+    // Check calculations are correct
+    const input = nn.toInput([1,1,1])
+    nn.ihWeights = [[0.9,0.8,0.7],[0.6,0.5,0.4]]
+    const output = nn.feedForward(input, nn.ihWeights, nn.ihBias)
+    const exp = [0.9677, 0.9241]
+    output.forEach((x, i) => eq(math.round(x[0], 4), exp[i]))
+    // Check output is expected shape
+    output.forEach(x => eq(1, x.length))
+    eq(nn.ihWeights.length, output.length)
+    eq(1, output[0].length)
+
+    // Check calculations are correct
+    const input2 = nn.toInput([1,1])
+    nn.hoWeights = [[0.9,0.8],[0.6,0.5]]
+    const output2 = nn.feedForward(input2, nn.hoWeights, nn.hoBias)
+    const exp2 = [0.9370, 0.8909]
+    output2.forEach((x, i) => eq(math.round(x[0], 4), exp2[i]))
+    // Check output is expected shape
+    output2.forEach(x => eq(1, x.length))
+    eq(nn.hoWeights.length, output2.length)
+    eq(1, output2[0].length)
+  },
+
+  "predict": () => {
+    let nn = NeuralNetwork(3, 2, 1)
+    let input = nn.toInput([1,1,1])
+    nn.ihWeights = [[0.9,0.8,0.7],[0.6,0.5,0.4]]
+    nn.hoWeights = [[0.9,0.8]]
+    let output = nn.predict(input)
+    eq(nn.hoWeights.length, output.length)
+    output.forEach(x => eq(1, x.length))
+    eq(0.9315, math.round(output[0][0], 4))
+
+    nn = NeuralNetwork(3, 2, 2)
+    nn.ihWeights = [[0.9,0.8,0.7],[0.6,0.5,0.4]]
+    nn.hoWeights = [[0.9,0.8],[0.6,0.5]]
+    output = nn.predict(input)
+    eq(nn.hoWeights.length, output.length)
+    output.forEach(x => eq(1, x.length))
+    eq(0.9315, math.round(output[0][0], 4))
+    eq(0.8852, math.round(output[1][0], 4))
+  },
+
+  "train": () => {
+    let nn = NeuralNetwork(3, 2, 1)
+    let input = nn.toInput([1,-1,1])
+    let target = nn.toInput([0.5])
+    nn.ihWeights = [[0.9,-0.8,0.7],[0.6,0.5,-0.4]]
+    nn.hoWeights = [[-0.9,0.8]]
+    
+    nn.train(input, target)
+
+    const ihWeightsExp = [[0.8835, -0.7835, 0.6835], [0.6112, 0.4888, -0.3888]]
+    ihWeightsExp.forEach((row, i) => row.map((x, j) => eq(x, math.round(nn.ihWeights[i][j], 4))))
+    const ihBiasExp = [[0.9835], [1.0112]]
+    ihBiasExp.forEach((row, i) => row.map((x, j) => eq(x, math.round(nn.ihBias[i][j], 4))))
+    const hoWeightsExp = [[-0.8870, 0.8090]]
+    hoWeightsExp.forEach((row, i) => row.map((x, j) => eq(x, math.round(nn.hoWeights[i][j], 4))))
+    const hoBiasExp = [[1.0134]]
+    hoBiasExp.forEach((row, i) => row.map((x, j) => eq(x, math.round(nn.hoBias[i][j], 4))))
+  },
 }
